b86249efda8a22bd70e04e140e55543a9421ddb0
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-03 14:59:41 +0100

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("Size of float: %d byte(s)\n", sizeof(f));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
