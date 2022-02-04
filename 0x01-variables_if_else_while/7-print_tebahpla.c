b79dec00837e206ac4e556ab8b12e3089baa6ca1
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 17:21:56 +0100

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always (0)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i++)
+	{
+	putchar (i);
+	}
+	putchar('\n');
+	return (0);
+}
