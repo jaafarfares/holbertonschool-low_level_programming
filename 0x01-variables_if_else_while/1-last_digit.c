87e5771f1f4de4c70753a7a09d5545c629996421
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 13:04:27 +0100

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function maqin goes there */
+/**
+ * main - Entry point
+ * Return: Always (0)
+ *
+ */
 int main(void)
-{	
+{
 	int n;
 	int LD;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lD = n % 10;
-	if(LD > 5)
-	  printf("Last digit of %d is %d and is greater than 5\n", (n), (LD));
-	if else (LD = 0)
-		  printf("Last digit of %d is %d and is 0\n", (n), (LD));
-	if else(LD < 6 && LD ! = 0)
-		 printf("Last digit of %d is %d and is less than 6 and not 0\n" , (n), (LD));
+	LD = n % 10;
+	if (LD > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", (n), (LD));
+	else if (LD == 0)
+		printf("Last digit of %d is %d and is 0\n", (n), (LD));
+	else if (LD < 6 && LD != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", (n), (LD));
 	return (0);
 }
