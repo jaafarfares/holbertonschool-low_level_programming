226fd8616ac04d14f57b6c7344771c8239637cdc
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 16:58:25 +0100

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always (0)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+	putchar('0' + n);
+	}
+	putchar('\n');
+	return (0);
+}
