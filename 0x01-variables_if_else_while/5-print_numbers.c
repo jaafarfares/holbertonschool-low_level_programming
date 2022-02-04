4b8119e6e051ff2b2f779a93291648c28d3292d5
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 16:20:12 +0100

    10-0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always (0)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+	printf("%d", (n));
+	}
+	printf("\n");
+	return (0);
+}
