23d9ef7c1c4864e52a48e2ae4e1aecf0b53daa7d
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 15:50:13 +0100

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'q' && n != 'e')
+			putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
