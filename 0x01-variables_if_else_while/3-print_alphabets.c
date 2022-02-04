d95f9eaba549975a31d9fd9bdff75847b4232bcc
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 14:54:15 +0100

    for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+File Edit Options Buffers Tools C Help
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char n, m;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	putchar(n);
+	}
+	for (m = 'A' ; m <= 'Z' ; m++)
+	{
+	putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
