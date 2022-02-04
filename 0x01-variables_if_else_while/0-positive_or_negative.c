ce690704207549b2ad80c873155b3f921f10747a
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-04 12:25:44 +0100

    is positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always (0)
+ */
 int main(void)
 {
 	int n;
@@ -9,10 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf ("%d is positive\n" , (n));
-	else if(n=0)
-	printf("%d is zero\n" , (n));
-	else if(n < 0)
-	printf("%d is negative\n" , (n));
+	printf("%d is positive\n", (n));
+	else if (n == 0)
+	printf("%d is zero\n", (n));
+	else if (n < 0)
+	printf("%d is negative\n", (n));
 	return (0);
 }
