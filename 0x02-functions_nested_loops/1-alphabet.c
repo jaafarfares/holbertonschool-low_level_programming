0b0c4233b0426d357b4da7a349fd68525817e8c9
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-08 16:34:51 +0100

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet - writes the character c to stdout
- * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -10,10 +8,9 @@ void print_alphabet(void)
 {
 	char n;
 
-	for (n = 97; n <= 122; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{
 	_putchar(n);
-	}
-
+	}						        
 	_putchar('\n');
 }
