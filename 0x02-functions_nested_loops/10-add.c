dcd195cc564420121834a312a08e2cd170ab5921
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-09 12:25:31 +0100

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add - check the code
+* @a: input
+* @b: input
+* Return: Always 0.
+*
+*/
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
