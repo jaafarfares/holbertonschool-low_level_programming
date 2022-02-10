263250c87349635784f60829c8bd09c6d824b35f
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-10 16:57:43 +0100

    Write a function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - check the code
+*@n: input
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+	int sum;
+
+	if (n <= 0)
+	{
+		for (sum = 0; sum < n; sum++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
