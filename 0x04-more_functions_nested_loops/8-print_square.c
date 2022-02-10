527c8db6a1e9ed11924ae5d2f915e2eb95b86a84
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-10 19:41:49 +0100

    Write a function that prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_square - check the code
+*@size: input
+* Return: Always 0.
+*/
+void print_square(int size)
+{
+	int lg;
+
+	if (a >= b && a >= c)
+	{
+		lg = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		lg = b;
+	}
+	else
+	{
+		lg = c;
+	}
+	return (lg);
+}
