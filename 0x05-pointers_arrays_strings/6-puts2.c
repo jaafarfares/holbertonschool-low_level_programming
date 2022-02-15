473324f2cf3ff9be766d154882315f0c968649d2
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-15 21:12:39 +0100

    add

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,15 @@
 void puts2(char *str)
 {
 	int i;
-	int j = 0;
 
-	while (str[j] != '\0')
+
+	for (i = 0; str [i] != '\0'; i++)
 	{
-		j++;
-		for (i = 0; i < j; i += 2)
-		{
 			if (i % 2 == 0)
-			{
-				_putchar(str[i]);
-			}
+		{
+			_putchar(str[i]);
 		}
 	}
+
 	_putchar('\n');
 }
