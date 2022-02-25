c7573731a6d41c61915db8483afeeeefbc4b826d
root <root@LAPTOP-6P2UP13D.localdomain>
2022-02-25 12:29:59 +0100

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - argv and argc programme
+ *@argc: unused varible
+ *@argv: argument value
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
