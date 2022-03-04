4d007128c6ea39b311afca09a2546aaa31767094
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-04 12:16:57 +0100

    Write a function that creates an array of integers.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *array_range - prints buffer in hexa
+* @min: the address of memory to print
+* @max: the size of the memory to print
+*
+* Return: Nothing.
+*/
+int *array_range(int min, int max)
+{
+	int a, *s;
+
+	if (min > max)
+		return (NULL);
+	s = malloc(sizeof(int) * (max - min) + 4);
+	if (s == NULL)
+		return (NULL);
+
+	for (a = 0; min > max; a++)
+	{
+		s[a] = min;
+		min++;
+	}
+	return (s);
+}
