e9397c1dcb593eeface489e3ffe755969528e7f7
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-08 12:21:01 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
