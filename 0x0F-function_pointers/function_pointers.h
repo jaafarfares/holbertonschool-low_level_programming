05fb9208abf602564acd90044ac93808ee38f841
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-09 11:45:59 +0100

    function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_PINTERS_H
+#define FUNCTION_PINTERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include <math.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
+#endif
