52d29d0e063d05a50858ce39a88e2b670e6c63f3
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-21 11:51:50 +0100

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "lists.h"
+void print_Fun(void) __attribute__ ((constructor));
+/**
+* print_Fun - the function
+*Return: .....
+*/
+
+void print_Fun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
