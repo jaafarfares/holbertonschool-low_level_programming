c56cefaae01584201421136ab819a838bbf21708
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-22 13:25:18 +0100

    Write a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = NULL;
+	listint_t *tmp;
 
 	if (head == NULL)
 		return;
-	while (head != NULL)
+	tmp = *head;
+	while (tmp != NULL)
 	{
-		tmp = *head;
 		tmp = tmp->next;
 		free(tmp);
 	}
+	*head = NULL;
 }
