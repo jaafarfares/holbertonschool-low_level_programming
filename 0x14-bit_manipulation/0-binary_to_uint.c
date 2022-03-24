b4bb972ae75d6cf0a2ede42200c57d4b79fa045c
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-24 19:19:03 +0100

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal = 0;
 	int i = 0;
 
-	while (b[i] == '0' || b[i] == '1')
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
 	{
 		decimal <<= 1;
 		decimal += b[i] - '0';
 		i++;
-	}
-		if (b == NULL || b[i] != 1 || b[i] != 0)
-	{
-		return (0);
 	}
 		return (decimal);
 }
