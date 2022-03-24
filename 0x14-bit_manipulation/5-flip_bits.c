62c1682ef803c1c8f0aaa358629361722aea4c4e
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-24 22:25:33 +0100

    test

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - check the code
+ *@n: ...
+ *@m: .....
+ * Return: Always 0.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int count = 0;
+	int last_bit_n, last_bit_m;
+
+	while (n || m)
+	{
+	last_bit_n = n & 1;
+	last_bit_m = m & 1;
+	if (last_bit_n != last_bit_m)
+	count++;
+	}
+	return (count);
+}
