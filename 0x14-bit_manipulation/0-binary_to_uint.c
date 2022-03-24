cb5444f1be38d03b51abb1725951ac7ba840a381
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-24 14:38:14 +0100

    ...

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal = 0;
 	int i = 0;
 
-	if (b == NULL)
-	return (0);
-	if (b[i] != '0' || b[i] != '1')
-		return (0);
 	while (b[i] == '0' || b[i] == '1')
 	{
 		decimal <<= 1;
 		decimal += b[i] - '0';
 		i++;
+	}
+	if (b == NULL)
+		return (0);
+	else
+	{
+		return (0);
 	}
 	return (decimal);
 }
