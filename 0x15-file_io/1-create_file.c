3d65892faaf72810faca36396f9b22bb07370dd4
root <root@LAPTOP-6P2UP13D.localdomain>
2022-03-30 13:25:15 +0100

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	ftp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	ftp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	if (ftp == -1)
 		return (-1);
 	if (text_content == NULL)
