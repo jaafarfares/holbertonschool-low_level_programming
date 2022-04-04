bf57bb477052c3d66e6851a9e8baf6297c801d22
root <root@LAPTOP-6P2UP13D.localdomain>
2022-04-04 14:36:56 +0100

    test

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <math.h>
+
+/**
+ * print_number - ....
+ *@n: ....
+ *Return: ....
+ */
+void print_number(int n)
+{
+	if (n == '0')
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		}
+	if (n != 0)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}
