ef5a4cba3ce62ee35ca611b7826e5dc430375350
jaafarfares <jaafarferes@gmail.com>
2022-08-30 08:48:07 +0100

    0. Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ *int linear_search - ....
+ * @array: ...
+ * @size: ...
+ * @valye: .....
+ * Return: ....
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%li] = [%i]\n", i, array[i]);
+            if (array[i] == value)
+                return (i);
+    }
+    return (-1);
+}
