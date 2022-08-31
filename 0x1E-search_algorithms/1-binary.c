2149e0a0bfac9912f19084d3a86ff50daf3861d0
jaafarfares <jaafarferes@gmail.com>
2022-08-31 08:45:59 +0100

    binary search algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,33 +10,36 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t mid, i, left = 0, right = size -1;
+	size_t mid, i, left = 0;
+	size_t right = size - 1;
 
-
-    if (array == NULL)
+	if (array == NULL)
 	{
 		return (-1);
-    }
-    while(left <= right)
-    {
-        printf("Searching in array: ");
+	}
+	while (left <= right)
+	{
+		printf("Searching in array: ");
 
-		for (i = left; i < right ; i++)
+		for (i = left; i < right; i++)
 		{
 			printf("%i, ", array[i]);
 		}
 
 		printf("%i\n", array[right]);
 
-        mid = (left + right) / 2;   
+		mid = (left + right) / 2;
 
-        if (array[mid] == value)
-            return mid;
-
-        else if (value < array[mid])
-            right = mid - 1;
-        else
-            left = mid + 1;
-    }
-    return (-1);
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (value < array[mid])
+		{
+			right = mid - 1;
+		}
+		else
+			left = mid + 1;
+	}
+	return (-1);
 }
